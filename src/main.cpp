8d455f39e5c7e9c42c77230e17b16c4417e66345
botamochi6277 <botamochi6277@icloud.com>
2024-01-28 01:48:58 +0900

    Add fbk_face

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
+
 #include <ArduinoBLE.h>
 #include <Avatar.h>
-#include <ESP32Servo.h>
 #include <M5Unified.h>
+#include <faces/DogFace.h>
 
 #include "BLEStackchanService.hpp"
+#include "fbk_face.hpp"
 
+#ifdef FEETECH
+#include "SCServo.h"
+#else
+#include <ESP32Servo.h>
+#endif
 m5avatar::Avatar avatar;
 ble::StackchanService stackchan_srv;
-Servo servo_pan;
-Servo servo_tilt;
-uint8_t pan_min = 60;
-uint8_t pan_max = 120;
+
+// (todo) make servo manager
+#ifdef FEETECH
+SMS_STS st;
+#endif
+short max_sweep = 4095;
+short min_sweep = 0;
+unsigned short speed = 3400;
+unsigned char acc = 50;
+const uint8_t servo_pan_id = 0;
+const uint8_t servo_tilt_id = 1;
+uint8_t pan_min = 60;   // deg
+uint8_t pan_max = 120;  // deg
 uint8_t tilt_min = 80;
 uint8_t tilt_max = 100;
 
@@ -28,27 +44,26 @@ m5avatar::ColorPalette* color_palettes[4];
 const uint8_t color_palettes_size = 4U;
 uint8_t color_palettes_idx = 0;
 
-void sweepUpdate(Servo& servo, float t, float f, float amp = 90.0f,
-                 int offset = 90) {
-  int angle = static_cast<int>(amp * sin(2.0f * M_PI * f * t)) + offset;
-  servo.write(angle);
-}
+m5avatar::Face* faces[3];
+const int faces_length = sizeof(faces) / sizeof(m5avatar::Face*);
+int face_idx = 0;
 
 void setup() {
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
+#ifdef FEETECH
+  Serial1.begin(1000000, SERIAL_8N1, 19, 27);
+  st.pSerial = &Serial1;
+#endif
 
-  servo_pan.setPeriodHertz(50);   // standard 50 hz servo
-  servo_tilt.setPeriodHertz(50);  // standard 50 hz servo
-
-  stackchan_srv.pan_limit.min = 60;
-  stackchan_srv.pan_limit.max = 90;
-  stackchan_srv.tilt_limit.min = 80;
-  stackchan_srv.tilt_limit.max = 100;
+  stackchan_srv.pan_limit.min = 60 + 90;
+  stackchan_srv.pan_limit.max = 90 + 90;
+  stackchan_srv.tilt_limit.min = 80 + 90;
+  stackchan_srv.tilt_limit.max = 100 + 90;
 
   M5.begin();
+
+  faces[0] = avatar.getFace();
+  faces[1] = new m5avatar::DogFace();
+  faces[2] = new m5avatar::FbkFace();
 
   color_palettes[0] = new m5avatar::ColorPalette();
   color_palettes[1] = new m5avatar::ColorPalette();
@@ -65,7 +80,13 @@ void setup() {
 
   if (!BLE.begin()) {
     // "starting BLE failed!"
+    avatar.setSpeechText("BLE is unavailable");
   }
+#ifdef FEETECH
+  if (!Serial1.available()) {
+    avatar.setSpeechText("Serial1 is unavailable");
+  }
+#endif
 
   String ble_address = BLE.address();
   String local_name = "Stackchan_" + ble_address;
@@ -78,6 +99,15 @@ void setup() {
   stackchan_srv.setInitialValues();
   // start advertising
   BLE.advertise();
+
+#ifdef FEETECH
+  // init position
+  st.RegWritePosEx(servo_pan_id, max_sweep / 2, speed, acc);
+  st.RegWritePosEx(servo_tilt_id, max_sweep / 2, speed, acc);
+  st.RegWriteAction();
+  // delay(1884);  //[(P1-P0)/Speed]*1000+[Speed/(Acc*100)]*1000
+
+#endif
 }
 
 void loop() {
@@ -87,12 +117,17 @@ void loop() {
   milli_sec = millis();
   stackchan_srv.timer_chr.writeValue(milli_sec);
   BLE.poll();
-
-  stackchan_srv.servoPoll(servo_pan, servo_tilt);
+#ifdef FEETECH
+  stackchan_srv.servoPoll(st);
+#endif
   stackchan_srv.facialExpressionPoll(avatar, expressions, expressions_size);
   stackchan_srv.facialColorPoll(avatar, color_palettes, color_palettes_size);
   stackchan_srv.mouseOpenPoll(avatar);
 
+  if (M5.BtnA.wasPressed()) {
+    avatar.setFace(faces[face_idx]);
+    face_idx = (face_idx + 1) % faces_length;
+  }
   if (M5.BtnB.wasPressed()) {
     avatar.setColorPalette(*color_palettes[color_palettes_idx]);
     // have no effect on written flag
